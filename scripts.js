a8179ec3b8545135f7242ec22d5a98c881dbe358
Stigg101 <gustavheese@live.com>
2023-04-01 10:55:05 +0200

    removed script.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,2 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Select all the navigation links
-    let navLinks = document.querySelectorAll(".nav-link");
-  
-    // Add click event listeners to each navigation link
-    navLinks.forEach((link) => {
-      link.addEventListener("click", function (event) {
-        event.preventDefault();
-        let target = this.getAttribute("href");
-  
-        // Scroll to the target element smoothly
-        document.querySelector(target).scrollIntoView({
-          behavior: "smooth",
-        });
-      });
-    });
-  });
+
   
