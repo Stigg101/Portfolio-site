a37c50ad14de092de306e7719ec5dbe295b65d81
Stigg101 <gustavheese@live.com>
2023-04-02 19:42:45 +0200

    Added a fade in effect

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 
-  
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const projectCards = document.querySelectorAll(".fade-in");
+    
+    projectCards.forEach((card, index) => {
+      setTimeout(() => {
+        card.classList.add("visible");
+      }, index * 250);
+    });
+  });
